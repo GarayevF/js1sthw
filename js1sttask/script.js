ca91b0bc8e68c1903977f26f84a1ed14b9568a49
GarayevF <112488747+GarayevF@users.noreply.github.com>
2022-11-20 16:28:51 +0400

    Update script.js

--- js1sttask/script.js
+++ js1sttask/script.js
@@ -1,29 +1,29 @@
 // TASK 1
 
 
-var date = new Date();
+// var date = new Date();
 
-var title = document.getElementById("title");
-var time = date.getHours();
+// var title = document.getElementById("title");
+// var time = date.getHours();
 
-if (time >= 6 && time < 12) {
-    title.innerHTML = "Sabahın xeyir";
-    document.body.style.backgroundColor = "cyan";
-    alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nSabahdır");
-} else if (time >= 12 && time < 18) {
-    title.innerHTML = "Günortan xeyir";
-    document.body.style.backgroundColor = "orange";
-    alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nGünortadır");
-} else if (time >= 18 && time <= 24){
-    title.innerHTML = "Axşamın xeyir";
-    document.body.style.backgroundColor = "gray";
-    alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nAxşamdır");
-} else if (time >= 0 && time < 6){
-    title.innerHTML = "Gecən xeyir";
-    document.body.style.backgroundColor = "black";
-    alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nGecədir");
-    document.getElementById("title").style.color = "white";
-}
+// if (time >= 6 && time < 12) {
+//     title.innerHTML = "Sabahın xeyir";
+//     document.body.style.backgroundColor = "cyan";
+//     alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nSabahdır");
+// } else if (time >= 12 && time < 18) {
+//     title.innerHTML = "Günortan xeyir";
+//     document.body.style.backgroundColor = "orange";
+//     alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nGünortadır");
+// } else if (time >= 18 && time <= 24){
+//     title.innerHTML = "Axşamın xeyir";
+//     document.body.style.backgroundColor = "gray";
+//     alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nAxşamdır");
+// } else if (time >= 0 && time < 6){
+//     title.innerHTML = "Gecən xeyir";
+//     document.body.style.backgroundColor = "black";
+//     alert("Saat : " + date.getHours() + ":" + date.getMinutes() + "\nGecədir");
+//     document.getElementById("title").style.color = "white";
+// }
 
 
 
